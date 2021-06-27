2db062997143e601e9bba828c3fd7f5ce8048ea1
Guilherme_Nome_Commit <guilherme_nome_commit@gmail.com>
2021-06-27 18:07:56 -0300

    Implementando o DarkMode
    Arquivo darkmode criado, funcionando, porém não faz transição entre telas, implementar nos próximos commits

--- public/scripts/darkmode.js
+++ public/scripts/darkmode.js
@@ -1,9 +1,29 @@
 // Dark Mode
 const html = document.querySelector('html')
-const asd = document.querySelector(".bg")
-console.log(asd)
+// const asd = document.querySelector(".bg")
+const checkbox = document.querySelector('.switch input')
+const rocketLogo = document.querySelector('.rocket-logo')
+const darkModeText = document.querySelector('#dark-mode-text')
+var darkMode = false;
 
+checkDarkMode()
 
-// checkbox.addEventListener('change', function() {
-//     html.classList.toggle('dark')
-// })
+checkbox.addEventListener('change', function() {
+    checkDarkMode()
+    // html.classList.toggle('dark')
+})
+
+function checkDarkMode(){
+    darkMode = checkbox.checked 
+    console.log('darkmode: ' + darkMode)
+    
+    if (darkMode)
+    {
+        html.classList.add("dark")
+        rocketLogo.src = "/images/logo-dark.png"        
+    }
+    else{
+        html.classList.remove("dark")
+        rocketLogo.src = "/images/logo.svg"              
+    }
+}
