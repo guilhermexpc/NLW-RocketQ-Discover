c720f3b072b676319a6aa811252fc86090b8e9db
Guilherme_Nome_Commit <guilherme_nome_commit@gmail.com>
2021-06-27 18:54:58 -0300

    Dark Mode completo
    Agora o dark mode passa de uma tela para outra. mesmo que feche o site ele ainda continua

--- public/scripts/darkmode.js
+++ public/scripts/darkmode.js
@@ -7,22 +7,45 @@ const darkModeText = document.querySelector('#dark-mode-text')
 var darkMode = false;
 
 checkDarkMode()
+if (checkbox)
+{
+    checkbox.addEventListener('change', function() {
+        ativarDarkMode()
+        // html.classList.toggle('dark')
+    })
+    
+}
 
-checkbox.addEventListener('change', function() {
-    checkDarkMode()
-    // html.classList.toggle('dark')
-})
-
-function checkDarkMode(){
+function ativarDarkMode(){
     darkMode = checkbox.checked 
     console.log('darkmode: ' + darkMode)
-    
+    localStorage.setItem('darkmode', darkMode ? "on" :  "off")
+
     if (darkMode)
     {
         html.classList.add("dark")
         rocketLogo.src = "/images/logo-dark.png"        
     }
     else{
+        html.classList.remove("dark")
+        rocketLogo.src = "/images/logo.svg"              
+    }
+}
+
+function checkDarkMode(){
+    darkMode = localStorage.getItem('darkmode')
+    console.log('checkDarkMode: ' + darkMode)    
+    
+    if (darkMode == "on")
+    {
+        if (checkbox)
+            checkbox.checked = true
+        html.classList.add("dark")
+        rocketLogo.src = "/images/logo-dark.png"        
+    }
+    else{
+        if (checkbox)
+            checkbox.checked = false
         html.classList.remove("dark")
         rocketLogo.src = "/images/logo.svg"              
     }
